0946a187274468d5d999c1d2f01ea674fba5fe6d
Bo Chen <bochen2014@yahoo.com>
2016-10-08 04:25:13 +1100

    rewording before release

--- src/home/index.js
+++ src/home/index.js
@@ -4,11 +4,11 @@ const homePage = () => {
   return (
     <div style={{fontSize:'1.5em', marginTop:'2em'}}>
       Hi, this is a demo for the next gen backoffice, although the first generation hasn't been born yet...
-      <br/>I hope we can make backoffice better than it is now, , and it has never been a better time to refactor it now
+      <br/>I hope we can make backoffice better than it is today, , and it has never been a better time to refactor it now for below reasons
       <ul>
-        <li>it only contains three pages</li>
-        <li>we havn't started settlement page yet, so we have to do it anyway</li>
-        <li>the transaction page has design issues, and is very slow. we are re-factoring it</li>
+        <li>it only contains three pages (so far, hopefully)</li>
+        <li>we havn't started settlement page yet, so we would have to do it anyway</li>
+        <li>the transaction page has architect problems, and is very very slow. we are re-factoring it right now</li>
         <li>the merchant page doesn't really fit our database, so we customized it, and it's a very small page</li>
       </ul>
    
