f3f7a7ae132fa4c4244914e7ed649d1e2af50516
Bo Chen <bochen2014@yahoo.com>
2016-10-08 04:14:55 +1100

    for demo

--- src/home/index.js
+++ src/home/index.js
@@ -1,105 +1,21 @@
-import React, {Component} from 'react'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import {TextField,
-        RaisedButton,
-        CircularProgress
-        } from 'material-ui'
+import React from 'react'
 
-
-import searchApi from '../api/service'
-import SearchBox from '../common/searchBox'
-import SearchResult from './searchResult'
-
-const muiTheme = getMuiTheme()
-
-class SearchPage extends Component {
-  constructor(props) {
-    super(props)
-    this.onSearchTextChanged = this.onSearchTextChanged.bind(this)
-    this.setRef = this.setRef.bind(this)
-    this.state={
-      showResult : false,
-      loading: false,
-      searchQueued : false,
-      searchResult :[]
-    }
-  }
-
-  componentDidMount() {
-    this.search_input.focus()
-  }
-
-  executeSearch(){
-    // to cater for the case where use type too fast
-    // we only want to search once in such case
-    const keyword = this.search_input.getValue()
-    this.setState({
-              showResult: true,
-              loading:true
-          })
-    //console.log('begining search for keyword: ' + keyword)
-    searchApi.search(keyword).then((searchResult) => {
-      this.setState({
-        showResult: true,
-        loading:false,
-        searchResult
-      })
-     })
-
-     this.setState({searchQueued:false})
-  }
-
-  onSearchTextChanged() {
-    const {searchQueued} = this.state
-    if(!searchQueued){
-       setTimeout(this.executeSearch.bind(this), 800)
-    }
-    this.setState({searchQueued:true})
-  }
- 
- setRef(ref){
-   this.search_input = ref
- }
-
- getResult(){
-   const {showResult, loading, searchResult} = this.state
-   if(!showResult){
-     return null
-  }
-  if(loading){
-    return  (<div className='margin-top-40 col-xs-2 col-xs-offset-4'>
-          <CircularProgress />
-      </div>)
-  }
-
-  return (searchResult.length > 0 ?
-      searchResult.map((val,index)=>
-        <SearchResult key={'index+' + index} result={val} />)
-      :<div className='margin-top-10'>
-          <span>We currently don't have any results for your search, try another</span>
-      </div>)
- }
-
-  render() {
-    const {showResult, searchResult} = this.state
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div className="container">
-           <div className='row margin-top-10'>
-             <div className = 'col-xs-12 col-md-8 col-md-offset-1'>
-                <SearchBox  setRef={this.setRef} onChange={this.onSearchTextChanged}/>
-              </div>
-           </div>
-            <div className='row margin-top-10'>
-              <div className = 'col-xs-12 col-md-8 col-md-offset-1'>
-                 { this.getResult() }
-              </div>
-            </div>
-       </div>
-      </MuiThemeProvider>
-    )
-  }
+const homePage = () => {
+  return (
+    <div style={{fontSize:'1.5em', marginTop:'2em'}}>
+      Hi, this is a demo for the next gen backoffice, although the first generation hasn't been born yet...
+      <br/>I hope we can make backoffice better than it is now, , and it has never been a better time to refactor it now
+      <ul>
+        <li>it only contains three pages</li>
+        <li>we havn't started settlement page yet, so we have to do it anyway</li>
+        <li>the transaction page has design issues, and is very slow. we are re-factoring it</li>
+        <li>the merchant page doesn't really fit our database, so we customized it, and it's a very small page</li>
+      </ul>
+   
+      with this mock site (will do footer tmrw), 50% of work is already done. 
+      <br/><br/>i hope it's not too radical for us...
+    </div>
+  )
 }
 
-export default SearchPage
+export default homePage
--- src/home/searchResult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const searchResult =  ({result})=>(
-    <div name={result.id} className='margin-top-10'>
-        <a  target='_blank'  href={'http://' + result.siteUrl}>{result.siteUrl}</a>
-        <div>{result.description}</div>
-    </div>)
-
-export default searchResult
